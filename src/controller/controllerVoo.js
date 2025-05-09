e4d5643293a6da738a98146e4f5a81fb6df87125
batistmath <matheus.batista@estudante.ifgoiano.edu.br>
2025-05-09 17:54:03 -0300

    commit

--- src/controller/controllerVoo.js
+++ src/controller/controllerVoo.js
@@ -1,4 +1,5 @@
 const Voo = require('../model/modelVoo')
+const portaoEmbarque = require('../model/modelPortaoEmbarque')
 const express = require('express')
 
 const controller = express()
@@ -7,7 +8,7 @@ controller.use(express.json())
     exports.getvoo = async (req,res) => {
         const voos = await Voo.find()
         return res.send(voos)
-    },
+    }
     exports.savevoo = async (req,res) => {
         const voos = new Voo({
             numeroVoo: req.body.numeroVoo,
@@ -19,7 +20,33 @@ controller.use(express.json())
         })
         await voos.save()
         return res.send(voos)
-    },
+    }
+    exports.addportaovoo = async (req,res) => {
+        const {idVoo, idPortao} = req.params
+        const voos = await Voo.findByIdAndUpdate(idVoo, {
+            portaoId: req.body.portaoId
+        }, {
+            new:true
+        })
+        await portaoEmbarque.findByIdAndUpdate(idPortao, {
+            disponivel: false
+        }, {
+            new: true
+        })
+        return res.send(voos)
+    }
+    exports.vooconcluido = async (req,res) => {
+        const {idVoo, idPortao} = req.params
+        const voos = await Voo.findByIdAndUpdate(idVoo, {
+            status: req.body.status
+        }, {
+            new: true
+        })
+        await portaoEmbarque.findByIdAndUpdate(idPortao, {
+            disponivel: true
+        })
+        return res.send(voos)
+    }
     exports.editvoo = async (req,res) => {
         const voos = await Voo.findByIdAndUpdate(req.params.id, {
             numeroVoo: req.body.numeroVoo,
@@ -32,7 +59,7 @@ controller.use(express.json())
             new:true
         })
         return res.send(voos)
-    },
+    }
     exports.deletevoo = async (req,res) => {
         const voos = await Voo.findByIdAndDelete(req.params.id)
         return res.send(voos)
--- src/router/routerVoo.js
+++ src/router/routerVoo.js
@@ -6,5 +6,7 @@ router.post('/savevoo', voo.savevoo)
 router.get('/getvoo', voo.getvoo)
 router.put('/editvoo/:id', voo.editvoo)
 router.delete('/deletevoo/:id', voo.deletevoo)
+router.put('/vooconcluido/:idVoo/:idPortao', voo.vooconcluido)
+router.put('/addportao/:idVoo/:idPortao', voo.addportaovoo)
 
 module.exports = router;
